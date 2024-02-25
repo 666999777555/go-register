b09b4c7f35ee608e0b3a47fd3b094959043f048e
“仲鹏宇” <13086242+zhong-pengyuzpy@user.noreply.gitee.com>
2024-02-25 19:43:55 +0800

    grpc

---
+++ grpc/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func Register(port int) (*grpc.Server, error) {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen:%v", err)
+		return nil, err
+	}
+	s := grpc.NewServer()
+	//反射接口支持查询
+	reflection.Register(s)
+
+	log.Printf("sever listening at %v", err)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to listen:%v\",err")
+		return nil, err
+	}
+	return nil, err
+}
